29e2d995d143dcc74cc4713562da7b46f78c0b3e
Alireza <alireza.nasseh@gmail.com>
2025-06-04 21:53:08 +0200

    - Add json file to the project - Define TypeScript interfaces for Product, Variation, Attributes

---
+++ src/types/product.type.ts
@@ -0,0 +1,27 @@
+export interface ProductType {
+	id: string;
+	name: string;
+	images: string[][];
+	brand: string;
+	variations: VariationType[];
+	attributes: AttributesType;
+	min_reg_price: number;
+	max_reg_price: number;
+	min_price: number;
+	max_price: number;
+}
+
+interface VariationType {
+	name: string;
+	stock: number;
+	price: number;
+	regular_price: number;
+}
+
+interface AttributesType {
+	Size: string[];
+	Gender: string[];
+	"Main Accords": string[];
+	Brand: string[];
+	"type"?: string[];
+}
