354d233c92c908c51b3aab18c66cd000e9f70249
Alireza <alireza.nasseh@gmail.com>
2025-06-05 18:28:48 +0200

    - Add remote host config - Add lucide-react icons

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+		remotePatterns: [new URL('https://cdn4.beautinow.com/**')],
+	}
 };
 
 export default nextConfig;
