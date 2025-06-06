5ae7d1336bfcbe247e09fdaf82925beb9f3964f9
Alireza <alireza.nasseh@gmail.com>
2025-06-06 10:15:52 +0200

    - Add product image remote to config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-		remotePatterns: [new URL('https://cdn4.beautinow.com/**')],
+		remotePatterns: [
+			new URL('https://cdn4.beautinow.com/**'),
+			new URL('https://imagedelivery.net/**')
+		],
 	}
 };
 
